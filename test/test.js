59a21971ccaa45967fdeab37836d291f56a8dcec
Jimmy Tang <jtang@tchpc.tcd.ie>
2012-03-27 21:59:57 +0100

    tweak tests

--- test/test.js
+++ test/test.js
@@ -1,12 +1,9 @@
-
 var assert = require('assert');
 var data = require('../lib/dri');
 
 module.exports = {
-	'getSeries': function(done) {
+	'getSeries': function() {
 		var series = data.getAllSeries();
 		assert.isNotNull(series);
-
-		done();
 	}
 };
