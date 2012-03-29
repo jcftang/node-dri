5721bcaee92115ea8c1c6481e8bcfa9902e593b5
Quirijn Groot Bluemink <Quirijn.Groot.Bluemink@howest.be>
2012-03-29 17:20:05 +0100

    Added new tests

--- test/serial-item-creation.js
+++ test/serial-item-creation.js
@@ -3,18 +3,21 @@ var dri = require('dri');
 
 var collId = "";
 var seriesId = "";
-var rnd = Math.floor(Math.random()*101);
+var itemId = "";
+var arrItems = new Array()
+var rnd = Math.floor(Math.random()*11);
+var start = 1;
 module.exports = {
 	'createCollection': function(done) {
 		data = {};
 		data.name = "AutoTestColl"+rnd;
 		dri.createCollection(data,function(result){
-			console.log(result);
+			//console.log(result);
 			assert.isDefined(result);
 			collId = result;
 			done();
 		}, function(e){
-			console.log(e);
+			//console.log(e);
 			done();
 		});
 	},
@@ -25,35 +28,103 @@ module.exports = {
 			author:"AutoBot"
 			};
 		dri.createSeries(data,function(result){
-			console.log(result);
+			//console.log(result);
 			assert.isDefined(result);
 			seriesId = result
 			done();
 		}, function(e){
-			console.log(e);
+			//console.log(e);
 			done();
 		});
 	},
 	'createItem': function(done) {
-		data = {
-			parentId:seriesId,
-			Title:"AutoBotTitle"+rnd,
-			Subtitle:"AutoBotSubitle"+rnd
+		//setTimeout(function(){
+			data = {
+				parentId:seriesId,
+				Title:"AutoBotTitle"+rnd,
+				Subtitle:"AutoBotSubitle"+rnd,
+				objectId:rnd
 			};
-		for (var i=1, j=rnd; i <= j; i++) {
-			data.objectId = i;
 			dri.createItem(data,function(result){
-				console.log(result);
+				//console.log(i + " ------" + j);
 				assert.isDefined(result);
-				if( i = j)
+				itemId = result;
+				done();
+				
+			}, function(e){
+				if(arrItems.length == 2){
+					console.log(e);
 					done();
+				}
+			});
+		//}, 100);
+	},
+	'getItem': function(done) {
+		//setTimeout(function(){
+			dri.getItem(itemId,function(result){
+				console.log(result._id);
+				assert.eql(itemId, result._id);
+				done();
 			}, function(e){
 				console.log(e);
-				if( i = j)
+					assert.isDefined(e);
 					done();
 			});
-		};
-	}
+		//}, 100);
+	},
+	'getItems': function(done) {
+		//setTimeout(function(){
+			dri.getItems(seriesId,function(result){
+				str = result[0].parentId;
+				console.log(str);
+				assert.eql(str, seriesId);
+				done();
+			}, function(e){
+				console.log(e);
+				assert.isDefined(e);
+				done();
+			});
+		//}, 100);
+	},
+	'removeItem': function(done) {
+		//setTimeout(function(){
+			dri.removeItem(itemId, function(id){
+				assert.includes(itemId, id);
+				console.log("remove");
+				done();
+			}, function(){
+				console.log("errr");
+				done();
+			});
+		//}, 100);
+	},
+	'getAllRecordsByType': function(done) {
+		//setTimeout(function(){
+			dri.getAllRecordsByType("serie", function(data){
+				//console.log(data[0].type);
+				assert.eql(data[0].type, "serie");
+				done();
+			});
+		//}, 100);
+	},
+	'getAllMediaItems': function(done) {
+		//setTimeout(function(){
+			dri.getAllMediaItems(function(data){
+				//console.log(data[0]);
+				assert.isDefined(data);
+				done();
+			});
+		//}, 100);
+	}/*,
+	'findMedia': function(done) {
+		//setTimeout(function(){
+			dri.findMediaItem('4f71d50d000000580e000007',function(data){
+				//console.log(data[0].filename);
+				assert.isDefined(data);
+				done();
+			});
+		//}, 100);
+	}*/
 };
 
 
