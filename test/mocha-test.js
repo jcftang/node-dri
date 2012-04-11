674fc3b1e6d6578a8df2530f1f42d96dc16143fc
Quirijn Groot Bluemink <Quirijn.Groot.Bluemink@howest.be>
2012-04-11 11:01:37 +0100

    Migrated from Mongolian to mongoose

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -9,11 +9,15 @@ var arrItems = new Array();
 var rnd = Math.floor(Math.random() * 1001);
 
 describe('Test cases for node-dri package', function() {
-	describe('Creating a Collection', function() {
-		it('should create a collection and return the id of the collection', function(done) {
-			var data = {};
-			data.Title = "AutoTestColl" + rnd;
-			data.type = "collection";
+	describe('Creating a Dri-Collection', function() {
+		it('should create a Dri-Collection and return the id of the Dri-Collection', function(done) {
+			var data = {
+				properties:{
+					title:"AutoTestColl" + rnd,
+					subtitle:"AutoTestColl" + rnd
+				},
+				status : "Open"
+			};
 			dri.createCollection(data, function(result) {
 				result.should.be.ok
 				collId = result;
@@ -25,10 +29,11 @@ describe('Test cases for node-dri package', function() {
 	}), describe('Creating a Series', function() {
 		it('should create a series and return the id of the series', function(done) {
 			var data = {
-				collection : collId,
-				Title : "AutoTestSeries" + rnd,
-				author : "AutoBot",
-				type : "series"
+				properties:{
+					title:"AutoTestColl" + rnd,
+					subtitle:"AutoTestColl" + rnd
+				},
+				status : "Open"
 			};
 			dri.createSeries(data, function(result) {
 				result.should.be.ok
@@ -41,11 +46,11 @@ describe('Test cases for node-dri package', function() {
 	}), describe('Creating an Item', function() {
 		it('should create an Item and return the id of the Item', function(done) {
 			var data = {
-				parentId : seriesId,
-				Title : "AutoBotTitle" + rnd,
-				Subtitle : "AutoBotSubitle" + rnd,
-				objectId : rnd,
-				type : "item"
+				properties:{
+					title:"AutoTestColl" + rnd,
+					subtitle:"AutoTestColl" + rnd
+				},
+				status : "Approved"
 			};
 			dri.createItem(data, function(result) {
 				result.should.be.ok
@@ -58,13 +63,31 @@ describe('Test cases for node-dri package', function() {
 	}), describe('Getting an Item', function() {
 		it('should get an Item and return the Item', function(done) {
 			dri.getItem(itemId, function(result) {
-				should.equal(itemId, result._id);
+				assert.equal(itemId, result._id);
 				done();
 			}, function(e) {
 				should.not.exist(e);
 			});
 		})
-	}), describe('Inserting an Item', function() {
+	}), describe('Getting a Series', function() {
+		it('should get an Series and return the Series', function(done) {
+			dri.getSeries(seriesId, function(result) {
+				assert.equal(seriesId, result._id);
+				done();
+			}, function(e) {
+				should.not.exist(e);
+			});
+		})
+	}), describe('Getting a Dri-Collection', function() {
+		it('should get an Dri-Collection and return the Dri-Collection', function(done) {
+			dri.getCollection(collId, function(result) {
+				assert.equal(collId, result._id);
+				done();
+			}, function(e) {
+				should.not.exist(e);
+			});
+		})
+	}), /*describe('Inserting an Item', function() {
 		it('Should create the new item, update the position of the other items and return the id of the created item', function(done) {
 			var data = {
 				amount : 10,
@@ -111,7 +134,7 @@ describe('Test cases for node-dri package', function() {
 				done();
 			});
 		})
-	}), describe('Pushing the item into fedora', function() {
+	}), */describe('Pushing the item into fedora', function() {
 		it('should push the item into fedora and return the fedora id from that item', function(done) {
 			dri.approveItem(itemId, "cfedoraLib", function(pid) {
 				pid.should.include(":");
@@ -121,49 +144,42 @@ describe('Test cases for node-dri package', function() {
 				done();
 			});
 		})
-	}), describe('Getting items from a certain type', function() {
-		it('should return an array of items from a certain type (here series)', function(done) {
-			dri.getAllRecordsByType("series", function(data) {
-				should.equal(data[0].type, "series");
-				done();
-			});
-		})
-	}), describe('Removing an item and children items', function() {
+	}),describe('Removing an item and children items', function() {
 		it('should return the id of the removed item', function(done) {
 			dri.removeItem(itemId, function(id) {
-				should.equal(itemId, id);
+				should.equal(1, id);
 				done();
 			}, function(err) {
 				should.not.exist(e);
 				done();
 			});
 		})
-	}), describe('Removing a series and children items', function() {
+	}),describe('Removing a series and children items', function() {
 		it('should return the id of the removed series', function(done) {
-			dri.removeItem(seriesId, function(id) {
-				should.equal(seriesId, id);
+			dri.removeSeries(seriesId, function(id) {
+				should.equal(1, id);
 				done();
 			}, function(err) {
 				should.not.exist(e);
 				done();
 			});
 		})
-	}), describe('Removing a colection and children series or items', function() {
-		it('should return the id of the removed collection', function(done) {
-			dri.removeItem(collId, function(id) {
-				should.equal(collId, id);
+	}), describe('Removing a Dri-Collection and children series or items', function() {
+		it('should return the id of the removed Dri-Collection', function(done) {
+			dri.removeCollection(collId, function(id) {
+				should.equal(1, id);
 				done();
 			}, function(err) {
 				should.not.exist(e);
 				done();
 			});
 		})
-	}), describe('Getting all media files', function() {
+	})/*, describe('Getting all media files', function() {
 		it('should return an array with the media files metadata', function(done) {
 			dri.getAllMediaItems(function(data) {
 				should.exist(data);
 				done();
 			});
 		})
-	})
+	})*/
 })
