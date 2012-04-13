54d724b2231a5d52267ce3eff02ae2508812a0fc
Quirijn Groot Bluemink <Quirijn.Groot.Bluemink@howest.be>
2012-04-13 12:23:44 +0100

    Updated to conform single schema objects

--- test/mocha-test.js
+++ test/mocha-test.js
@@ -9,6 +9,17 @@ var arrItems = new Array();
 var rnd = Math.floor(Math.random() * 1001);
 
 describe('Test cases for node-dri package', function() {
+	describe('Get object types', function() {
+		it('should return an array of all the object types', function(done) {
+			dri.getObjectTypes(function(result) {
+				console.log(result)
+				result.should.be.ok
+				done();
+			}, function(e) {
+				should.not.exist(e);
+			});
+		})
+	}),
 	describe('Creating a Dri-Collection', function() {
 		it('should create a Dri-Collection and return the id of the Dri-Collection', function(done) {
 			var data = {
@@ -31,8 +42,8 @@ describe('Test cases for node-dri package', function() {
 		it('should create a series and return the id of the series', function(done) {
 			var data = {
 				properties : {
-					title : "AutoTestColl" + rnd,
-					subtitle : "AutoTestColl" + rnd
+					title : "AutoTestSeries" + rnd,
+					subtitle : "AutoTestSeries" + rnd
 				},
 				status : "Open",
 				parentId: collId
@@ -50,10 +61,10 @@ describe('Test cases for node-dri package', function() {
 		it('should create an Item and return the id of the Item', function(done) {
 			var data = {
 				properties : {
-					title : "AutoTestColl" + rnd,
-					subtitle : "AutoTestColl" + rnd
+					title : "AutoTestItem" + rnd,
+					subtitle : "AutoTestItem" + rnd
 				},
-				status : "Approved",
+				status : "Open",
 				parentId: seriesId
 			};
 			dri.createItem(data, function(result) {
@@ -67,7 +78,7 @@ describe('Test cases for node-dri package', function() {
 		})
 	}), describe('Getting an Item', function() {
 		it('should get an Item and return the Item', function(done) {
-			dri.getItem(itemId, function(result) {
+			dri.getObject(itemId, function(result) {
 				assert.include(result._id,itemId)
 				done();
 			}, function(e) {
@@ -76,7 +87,7 @@ describe('Test cases for node-dri package', function() {
 		})
 	}), describe('Getting a Series', function() {
 		it('should get an Series and return the Series', function(done) {
-			dri.getSeries(seriesId, function(result) {
+			dri.getObject(seriesId, function(result) {
 				assert.equal(seriesId, result._id);
 				done();
 			}, function(e) {
@@ -94,7 +105,7 @@ describe('Test cases for node-dri package', function() {
 		})
 	}), describe('Getting a Dri-Collection', function() {
 		it('should get an Dri-Collection and return the Dri-Collection', function(done) {
-			dri.getCollection(collId, function(result) {
+			dri.getObject(collId, function(result) {
 				assert.equal(collId, result._id);
 				done();
 			}, function(e) {
@@ -113,7 +124,7 @@ describe('Test cases for node-dri package', function() {
 		})
 	}), describe('Removing the item ', function() {
 		it('should remove the item from MongoDB', function(done) {
-			dri.removeItem(itemId, function(result) {
+			dri.removeObject(itemId, function(result) {
 				assert.include(result,itemId)
 				done();
 			}, function(err) {
@@ -123,7 +134,7 @@ describe('Test cases for node-dri package', function() {
 		})
 	}), describe('Removing the series ', function() {
 		it('should remove the series from MongoDB', function(done) {
-			dri.removeSeries(seriesId, function(result) {
+			dri.removeObject(seriesId, function(result) {
 				assert.include(result,seriesId)
 				done();
 			}, function(err) {
@@ -133,7 +144,7 @@ describe('Test cases for node-dri package', function() {
 		})
 	}), describe('Removing the dri-collection ', function() {
 		it('should remove the dri-collection from MongoDB', function(done) {
-			dri.removeCollection(collId, function(result) {
+			dri.removeObject(collId, function(result) {
 				assert.include(result,collId)
 				done();
 			}, function(err) {
